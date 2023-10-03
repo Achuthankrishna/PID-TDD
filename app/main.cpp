79a63237e5902c2df70370b65d7f13abfee0ba3a
Jerry Pittman, Jr <jpittma1@umd.edu>
2023-10-03 17:42:33 -0400

    added doxy to main.cpp; updated cmake

--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,28 @@
+/**
+ * @file main.cpp
+ * @author Jerry Pittman, Jr. (jpittma1@umd.edu)
+ * @brief PID-TDD Main File
+ * @version 0.1
+ * @date 2023-10-03
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
 #include "../include/PID.hpp"
 #include <cmath>
 #include <iostream>
 
+/**
+ * @brief Construct a new PID::PID object
+ * 
+ * @param K_p proportional gain
+ * @param K_i integral gain
+ * @param K_d derivative gain
+ * @param d_t change in time
+ * @param current_state starting state
+ * @param target_state output/end goal
+ */
 PID::PID(double K_p, double K_i, double K_d,
 double d_t, double current_state, double target_state) {
     Kp = K_p;
@@ -13,4 +34,9 @@ double d_t, double current_state, double target_state) {
     curerr = 0;
     prevError = 0;
     integral = 0;
+}
+
+int main () {
+
+    return 0;
 }
