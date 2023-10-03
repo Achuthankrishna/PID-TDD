ff52b4ffbac66e334b35ae4ae2e2b5454ea54091
Achuthankrishna <achkrish07@gmail.com>
2023-10-03 17:49:58 -0400

    Added author to the driver code

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,7 @@
 /**
  * @file main.cpp
  * @author Jerry Pittman, Jr. (jpittma1@umd.edu)
+ *         Vyshnav Achuthan (vyachu07@umd.edu)
  * @brief PID-TDD Main File
  * @version 0.1
  * @date 2023-10-03
--- include/PID.hpp
+++ include/PID.hpp
@@ -1,7 +1,8 @@
 /**
  * @file PID.hpp
  * @author Jerry Pittman, Jr. (jpittma1@umd.edu)
- * @brief PID controller PID-TDD Project
+ *         Vyshnav Achuthan (vyachu07@umd.edu)
+ * @brief 
  * @version 0.1
  * @date 2023-10-03
  * 
